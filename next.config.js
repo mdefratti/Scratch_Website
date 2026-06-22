420dc2a5db5c47b44dd9097b9385cf96731ea2df
Marissa DeFretti <marissa.defratti@gmail.com>
2026-06-21 21:28:18 -0700

    Set basePath to /Scratch_Website for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  basePath: process.env.BASE_PATH ?? '',
+  basePath: '/Scratch_Website',
 }
 
 module.exports = nextConfig
